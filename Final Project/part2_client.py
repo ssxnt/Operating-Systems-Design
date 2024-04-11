ea4956afdc5421fb3d98b6fa49f9e8a484114a51
Sant <ssants21cf@gmail.com>
2024-04-11 14:35:52 -0700

    part 2 works

--- Final Project/part2_client.py
+++ Final Project/part2_client.py
@@ -8,89 +8,101 @@ import socket
 import threading
 from multiprocessing import current_process  # required for getting the current process name
 
+
 class ChatClient:
     """
     This class implements the chat client.
     It uses the socket module to create a TCP socket and to connect to the server.
     It uses the tkinter module to create the GUI for the chat client.
     """
+
     # To implement
-    def __init__(self, guiScreen, clientName=current_process().name):
+    def __init__(self, guiScreen, name=current_process().name):
+        """
+        Sets up the socket and the GUI
+        """
         self.guiScreen = guiScreen
-        self.clientName = clientName
-        self.clientRunning = True
+        self.clientName = name
 
         self.socketSetUp()
         self.guiSetup()
 
-        self.guiScreen.protocol("WM_DELETE_WINDOW", self.terminateConnection)
-        
     def socketSetUp(self):
+        """
+        Sets up the client socket and connects to server
+        """
         self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.clientSocket.bind(('127.0.0.1', 0))
-        self.address = self.clientSocket.getsockname()
-        self.clientSocket.connect(('127.0.0.1', 11111))
+        self.clientSocket.bind(('localhost', 0))
+        self.connection, self.address = self.clientSocket.getsockname()
+        self.clientSocket.connect((self.connection, 11111))
+        self.clientRunning = True
 
-        threading.Thread(target=self.rcvMessage, daemon=True).start()
+        threading.Thread(target=self.receiveMessage).start()
 
     def guiSetup(self):
-        self.guiScreen.title(f"Chat {self.clientName}")
-        
+        self.guiScreen.title(f"{self.clientName}")
+
+        self.clientLabel = Label(self.guiScreen, text=f"{self.clientName} @port #{self.address}",
+                                 font=("Courier", 11), anchor='w')
+        self.clientLabel.pack(fill='x')
+
         self.guiFrame = Frame(self.guiScreen)
         self.guiFrame.pack(fill='x')
 
-        self.chatEntry = Label(self.guiFrame, text="Message", font=("Courier", 11))
+        self.chatEntry = Label(self.guiFrame, text="Chat Message:", font=("Courier", 11))
         self.chatEntry.grid(row=0, column=0)
 
-        self.chatEntry = Entry(self.guiFrame, width=50)
-        self.chatEntry.grid(row=0, column=1)
-        self.chatEntry.bind('<Return>', self.sendMessage)
+        self.messageEntry = Entry(self.guiFrame, width=70)
+        self.messageEntry.grid(row=0, column=1)
+        self.messageEntry.bind('<Return>', self.sendMessage)
 
-        self.chatHistoryLabel = Label(self.guiScreen, text="Chat Window:", font=("Courier", 11))
+        self.chatHistoryLabel = Label(self.guiScreen, text="Chat History:", font=("Courier", 11), anchor='w')
         self.chatHistoryLabel.pack(fill='x')
 
         self.chatHistory = Text(self.guiScreen, state='disabled', font=("Courier", 11))
         self.chatHistory.pack(side='left', fill='both', expand=True)
-
-        self.chatHistory.tag_configure('center', justify='center')
+        self.chatHistory.tag_configure('left', justify='left')
 
     def sendMessage(self, event=None):
-        msg = self.chatEntry.get()
-        if msg:
-            self.clientSocket.sendall(msg.encode('utf-8'))
-            self.chatEntry.delete(0, END)
-            self.displayMessage(f"You: {msg}", 'center')
+        """
+        Sends a message to the server
+        """
+        messageEntered = self.messageEntry.get()
+        self.messageEntry.delete(0, END)
+        messageSending = f'{self.clientName}: {messageEntered}'
+        self.clientSocket.send(messageSending.encode('utf-8'))
+        self.displayMessage(messageSending, 'center')
 
-    def rcvMessage(self):
+    def receiveMessage(self):
+        """
+        Receives messages from the server and updates the chat message GUI
+        """
         while self.clientRunning:
             try:
-                msg = self.clientSocket.recv(1024).decode('utf-8')
-                if msg:
-                    self.displayMessage(msg, 'left')
-            except Exception:
-                print(f"Connection closed by server due to {Exception}")
-                self.terminateConnection()
+                messageReceived = self.clientSocket.recv(1024).decode('utf-8')
+                self.displayMessage(messageReceived, 'left')
+            except(ConnectionError):
+                self.clientRunning = False
+                self.clientSocket.close()
+                self.guiScreen.destroy()
+                break
+        self.clientSocket.close()
 
-    def displayMessage(self, msg, align):
-        self.chatHistory.config(state=NORMAL)
-        self.chatHistory.insert('end', msg + '\n', align)
-        self.chatHistory.config(state=DISABLED)
-        self.chatHistory.yview(END)
+    def displayMessage(self, messageToDisplay, place):
+        """
+        Displays the chat message on the GUI.
+        """
+        self.chatHistory.configure(state='normal')
+        self.chatHistory.insert(END, messageToDisplay + '\n', place)
+        self.chatHistory.configure(state='disabled')
 
-    def terminateConnection(self):
-        self.clientRunning = False
-        try:
-            self.clientSocket.close()
-        except Exception:
-            print(f"Error closing socket: {Exception}")
-        self.guiScreen.destroy()
 
-def main(): #Note that the main function is outside the ChatClient class
+def main():  # Note that the main function is outside the ChatClient class
     window = Tk()
     c = ChatClient(window)
     window.mainloop()
-    #May add more or modify, if needed 
+    # May add more or modify, if needed
 
-if __name__ == '__main__': # May be used ONLY for debugging
+
+if __name__ == '__main__':  # May be used ONLY for debugging
     main()
-    
